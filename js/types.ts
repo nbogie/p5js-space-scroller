8ea451a4c16186dbd7bfd68dec9261bc2d7e8d7b
nbogie <neillbogie@googlemail.com>
2021-09-12 15:37:38 +0100

    move interfaces into types.ts

---
+++ js/types.ts
@@ -0,0 +1,105 @@
+
+interface Asteroid {
+    live: boolean;
+    pos: p5.Vector;
+    vel: p5.Vector;
+    resType: ResourceType;
+    sizeCategory: number;
+    radius: number;
+    damage: number;
+    hp: number;
+    tookDamage: boolean;
+    rotation: number;
+    rotationSpeed: number;
+}
+interface AsteroidOpts {
+    sizeCategory?: number;
+    pos: p5.Vector;
+}
+interface Collidable {
+    pos: p5.Vector;
+    radius: number;
+}
+
+interface Orb {
+    pos: p5.Vector;
+    vel: p5.Vector;
+    live: boolean;
+    life: number;
+    radius: number;
+    exploding: boolean;
+}
+
+interface OrbOptions {
+    pos: p5.Vector;
+    vel: p5.Vector;
+}
+interface Palette {
+    colors: p5.Color[];
+    name: string;
+    url: string;
+}
+interface Particle {
+    pos: p5.Vector;
+    vel: p5.Vector;
+    radius: number;
+    color: p5.Color;
+    life: number;
+    hue: number;
+}
+interface ResourceType {
+    label: string;
+    hue: number;
+    color: p5.Color;
+}
+interface Shot {
+    live: boolean;
+    pos: p5.Vector;
+    rotation: number;
+    vel: p5.Vector;
+    radius: number;
+    damage: number;
+    color: p5.Color;
+    life: number;
+}
+interface ShotOptions {
+    vel: p5.Vector;
+    pos: p5.Vector;
+}
+interface Star {
+    pos: p5.Vector;
+    radius: number;
+    strength: number;
+}
+interface Target {
+    pos: p5.Vector;
+    live: boolean;
+}
+interface Trail {
+    particles: Particle[];
+}
+interface Vehicle {
+    pos: p5.Vector;
+    vel: p5.Vector;
+    accel: p5.Vector;
+    live: boolean;
+    life: number;
+    radius: number;
+    hp: number;
+    fuel: number;
+    maxSteeringForce: number;
+    facing: number;
+    maxSpeed: number;
+    hue: number;
+    color: p5.Color;
+    traction: 0.3;
+    steer: p5.Vector;
+    canShoot: boolean;
+    rammingDamage: number;
+    lastShot: number;
+    shotDelay: number;
+    trail: Trail;
+    target: Target;
+    desiredVector: p5.Vector;
+    tookDamage: boolean;
+}
